f5a81eb73847d0c437b69ebc048821571b728c09
Lucas Barros <lucas@cloudic.com.br>
2016-12-23 21:10:28 -0300

    Create install-wpscan.sh

---
+++ install-wpscan.sh
@@ -0,0 +1,5 @@
+sudo apt-get install libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+
+git clone https://github.com/wpscanteam/wpscan.git
+cd wpscan
+sudo gem install bundler && bundle install --without test
