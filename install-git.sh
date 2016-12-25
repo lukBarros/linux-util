3d8f2c93c821447d41cdbce2b4d3f212bbbb96d6
Lucas Barros <lucas@cloudic.com.br>
2016-12-24 23:05:55 -0300

    GIT E SMARTGIT GUI

--- install-git.sh
+++ install-git.sh
@@ -1,1 +1,14 @@
+### INSTALÇANDO GIT
+### REF https://git-scm.com/book/pt-br/v1/Primeiros-passos-Instalando-Git
+### https://www.digitalocean.com/community/tutorials/como-instalar-o-git-no-ubuntu-14-04-pt
 
+sudo apt-get update;
+sudo apt-get install git;
+
+### INSTALANDO UTILITARIOS
+### https://www.syntevo.com/smartgit/download
+
+sudo add-apt-repository ppa:eugenesan/ppa;
+sudo apt-get update;
+sudo apt-get install smartgit;
+sudo apt-get install smartgithg;
